41e9411e942f94c91b594d515b0cafdff52940cc
seung7 <seungmin.lee777@gmail.com>
2021-05-12 17:09:45 -0700

    original ORP-test code

--- vps_transmit.py
+++ vps_transmit.py
@@ -5,6 +5,14 @@ from array import array
 
 ser = Serial('/dev/ttyUSB0')
 # utf-8 encoding HU99\n, object type=HU(MAN), confidence=99(%)
-data = [72, 85, 57, 57, 10] 
+data = [72] 
 data = bytearray(data)
-ser.write(data)
+
+while True:
+    try:
+        ser.write(data)
+        print("transmission success")
+        break
+    except:
+        print("transmission to the bridege has failed")
+        
