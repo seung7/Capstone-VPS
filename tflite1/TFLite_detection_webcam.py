0c9f0f4d7c13f025a71ac4bcbed27ed6ae686720
seung7 <seungmin.lee777@gmail.com>
2021-06-11 16:24:22 -0700

    base64 works in webcam.py. Need to pass the variables to orp_test.py

--- tflite1/TFLite_detection_webcam.py
+++ tflite1/TFLite_detection_webcam.py
@@ -29,6 +29,7 @@ import importlib.util
 from picamera import PiCamera
 
 #added by Seungmin
+import base64
 #Adding the upper directory so that orp_test.py can communicate with this this file.
 sys.path.append('..')
 import test2
@@ -239,15 +240,15 @@ while True:
                 image=videostream.read()
 
                 #Convert Captured image to JPG(binary file) and save to buffer. Return value(retval) is set true if image is captured.
-               # retval, buffer =cv2.imencode('.jpg',image)
+                retval, buffer =cv2.imencode('.jpg',image)
 
                 #Convert to base64 encoding and show first 80 string info
-               # jpg_as_text = base64.b64encode(buffer)
-               # print(jpg_as_text[:80])
+                jpg_as_text = base64.b64encode(buffer)
+                print(jpg_as_text[:80])
 
                 #(No longer is used)Save the image file as 'opencv.png' save in directory. 
-                cv2.imwrite('opencv.png',image)
-                print("Human Detected. Image is saved")
+                #cv2.imwrite('opencv.png',image)
+                #print("Human Detected. Image is saved")
                 
     # Draw framerate in corner of frame
     cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
