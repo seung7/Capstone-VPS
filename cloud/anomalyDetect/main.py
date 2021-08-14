1b13b55c065702d8718821a766ee3cffaa15d19d
Raad Khan <raadkhan023@gmail.com>
2021-08-13 18:07:36 -0700

    update anomalyDetect limits and memory

--- cloud/anomalyDetect/main.py
+++ cloud/anomalyDetect/main.py
@@ -7,7 +7,7 @@ client = firestore.Client()
 
 frequency_profile = {
     'type': 'high',
-    'value': 24 # msgs/day
+    'value': 60 # msgs/day
 }    
 
 anomaly_profile = {
@@ -33,46 +33,38 @@ def anomalyDetect(data, context):
 
     sens_dps = client.collection(collection_path)
 
-    sens_dp_imei = data['value']['fields']['imei']['integerValue']
+    sens_dp_imei = int(data['value']['fields']['imei']['integerValue'])
     sens_dp_type = data['value']['fields']['type']['stringValue']
-    sens_dp_timestamp = data['value']['fields']['timestamp']['integerValue']
+    sens_dp_timestamp = int(data['value']['fields']['timestamp']['integerValue'])
     sens_dp_value = data['value']['fields']['value']['doubleValue']
     sens_dp_unit = data['value']['fields']['unit']['stringValue']
-
-    sens_dp = {
-        'imei': sens_dp_imei,
-        'type': sens_dp_type,
-        'timestamp': sens_dp_timestamp,
-        'value': sens_dp_value,
-        'unit': sens_dp_unit
-    }
     
-    present = datetime.fromtimestamp(int(sens_dp_timestamp)/1000)
-    past = (timedelta(days=-anomaly_profile['delta']) + present).timestamp()*1000
+    present = datetime.fromtimestamp(sens_dp_timestamp/1000)
+    past = int((timedelta(days=-anomaly_profile['delta']) + present).timestamp())*1000
     
     sens_dp_values = sens_dps.where(
+        'anomaly', '==', False).where(
         'imei', '==', sens_dp_imei).where(
         'type', '==', sens_dp_type).where(
         'timestamp', '>=', past).where(
-        'timestamp', '!=', sens_dp_timestamp).where(
-        'anomaly', '==', False).stream()
+        'timestamp', '!=', sens_dp_timestamp).stream()
 
     values = []
-    for sens_dp_value in sens_dp_values:
-        values.append(sens_dp_value.to_dict().get('value'))
+    for value in sens_dp_values:
+        values.append(value.to_dict().get('value'))
         
     if len(values) >= anomaly_profile['delta']*frequency_profile['value']: 
         avg = mean(values)
         stddev = pstdev(values)
-        up_limit = avg + 3*exp(stddev/avg)
-        lo_limit = avg - 3*exp(stddev/avg)
-        print(f'{up_limit} {avg} {lo_limit}')
+        up_limit = avg + 3*exp(10*stddev/avg)
+        lo_limit = avg - 3*exp(10*stddev/avg)
+        print(f'{up_limit} {avg} {stddev} {lo_limit}')
     else:
         print('calibrating...')
         return
         
-    if sens_dp['value'] < lo_limit or sens_dp['value'] > up_limit:
-        print(f"check out your property: detected an anomalous {sens_dp['type']} of {sens_dp['value']:.3f} {sens_dp['unit']} at {present}")
+    if sens_dp_value < lo_limit or sens_dp_value > up_limit:
+        print(f"check out your property: detected an anomalous {sens_dp_type} of {sens_dp_value:.3f} {sens_dp_unit} at {present}")
         sens_dps.document(document_path).set({'anomaly': True}, merge=True)
     else:
-        print(f"all is well at your property: {sens_dp['type']} was {sens_dp['value']:.3f} {sens_dp['unit']} just now")
+        print(f"all is well at your property: {sens_dp_type} was {sens_dp_value:.3f} {sens_dp_unit} just now")
